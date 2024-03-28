44273bf288ad6bc0b1936aecc108f4b34e59e39d
markerseeds <mark.see86@gmail.com>
2024-03-28 16:40:58 +0800

    implement login function on the frontend

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set Security HTTP headers
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -40,6 +41,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -64,6 +66,8 @@ app.use(
 // Test middleware
 app.use((req, res, next) => {
     req.currentTime = new Date().toISOString();
+    // eslint-disable-next-line no-console
+    console.log(req.cookies);
     next();
 });
 
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -10,14 +10,25 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findOne({ slug: req.params.slug }).populate({
         path: 'reviews',
         fields: 'review rating user',
     });
 
-    res.status(200).render('tour', {
-        title: `${tour.name} Tour`,
-        tour,
-    });
+    res.status(200)
+        .set(
+            'Content-Security-Policy',
+            'connect-src https://*.tiles.mapbox.com https://api.mapbox.com https://events.mapbox.com',
+        )
+        .render('tour', {
+            title: `${tour.name} Tour`,
+            tour,
+        });
 });
+
+exports.getLoginForm = (req, res) => {
+    res.status(200).render('login', {
+        title: 'Log into your account',
+    });
+};
