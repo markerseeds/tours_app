292d1fcd80b2d363b58fea31ac978a5da5f41ac8
markerseeds <mark.see86@gmail.com>
2024-03-29 11:12:45 +0800

    add account page

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,52 +1,50 @@
 /* eslint-disable */
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+export const displayMap = (locations) => {
+    mapboxgl.accessToken =
+        'pk.eyJ1IjoibWFya2Vyc2VlZHMiLCJhIjoiY2x1YXRreGhsMG9ucjJzcXhyNmp2ZG9hdCJ9.9FFGDN2z5zpgDpkiCUQQXg';
 
-mapboxgl.accessToken =
-    'pk.eyJ1IjoibWFya2Vyc2VlZHMiLCJhIjoiY2x1YXRreGhsMG9ucjJzcXhyNmp2ZG9hdCJ9.9FFGDN2z5zpgDpkiCUQQXg';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v10',
+        scrollZoom: false,
+        // center: [-118.113491, 34.111745],
+        // zoom: 10,
+        // interactive: false,
+    });
 
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10',
-    scrollZoom: false,
-    // center: [-118.113491, 34.111745],
-    // zoom: 10,
-    // interactive: false,
-});
+    const bounds = new mapboxgl.LngLatBounds();
 
-const bounds = new mapboxgl.LngLatBounds();
+    locations.forEach((loc) => {
+        // Create marker
+        const el = document.createElement('div');
+        el.className = 'marker';
 
-locations.forEach((loc) => {
-    // Create marker
-    const el = document.createElement('div');
-    el.className = 'marker';
+        // Add marker
+        new mapboxgl.Marker({
+            element: el,
+            anchor: 'bottom',
+        })
+            .setLngLat(loc.coordinates)
+            .addTo(map);
 
-    // Add marker
-    new mapboxgl.Marker({
-        element: el,
-        anchor: 'bottom',
-    })
-        .setLngLat(loc.coordinates)
-        .addTo(map);
+        // Add popup
+        new mapboxgl.Popup({
+            offset: 30,
+        })
+            .setLngLat(loc.coordinates)
+            .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+            .addTo(map);
 
-    // Add popup
-    new mapboxgl.Popup({
-        offset: 30,
-    })
-        .setLngLat(loc.coordinates)
-        .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-        .addTo(map);
+        // Extend map bounds to include current location
+        bounds.extend(loc.coordinates);
+    });
 
-    // Extend map bounds to include current location
-    bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-    padding: {
-        top: 200,
-        bottom: 150,
-        left: 100,
-        right: 100,
-    },
-});
-
-module.exports = { locations };
+    map.fitBounds(bounds, {
+        padding: {
+            top: 200,
+            bottom: 150,
+            left: 100,
+            right: 100,
+        },
+    });
+};
