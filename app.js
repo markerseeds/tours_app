49742485c0bb0b1af4f2fc3970a554796620444a
markerseeds <mark.see86@gmail.com>
2024-03-29 16:34:30 +0800

    add file upload functionality

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use(
 app.use((req, res, next) => {
     req.currentTime = new Date().toISOString();
     // eslint-disable-next-line no-console
-    console.log(req.cookies);
+    // console.log(req.cookies);
     next();
 });
 
