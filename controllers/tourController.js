06d933cccf8930bd3d2e94015d177a8e553ac11d
markerseeds <mark.see86@gmail.com>
2024-03-30 00:08:26 +0800

    add send email

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,17 +1,71 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
 
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true);
+    } else {
+        cb(
+            new AppError('Not an image! Please upload only images.', 400),
+            false,
+        );
+    }
+};
+
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+    { name: 'imageCover', maxCount: 1 },
+    { name: 'images', maxCount: 3 },
+]);
+
+// upload.single('image') req.file
+// upload.array('images', 5) req.files
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+    if (!req.files.imageCover || !req.files.images) return next();
+
+    // 1) Cover image
+    req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+    await sharp(req.files.imageCover[0].buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${req.body.imageCover}`);
+
+    // 2) Images
+    req.body.images = [];
+
+    await Promise.all(
+        req.files.images.map(async (file, i) => {
+            const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+            await sharp(file.buffer)
+                .resize(2000, 1333)
+                .toFormat('jpeg')
+                .jpeg({ quality: 90 })
+                .toFile(`public/img/tours/${filename}`);
+
+            req.body.images.push(filename);
+        }),
+    );
+
+    next();
+});
+
 exports.aliasTopTours = (req, res, next) => {
-    // req.query.limit = '5';
-    // req.query.sort = '-ratingsAverage,price';
-    // req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
-    req.query = {
-        limit: '5',
-        sort: '-ratingsAverage,price',
-        fields: 'name,price,ratingsAverage,summary,difficulty',
-    };
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
     next();
 };
 
@@ -29,7 +83,7 @@ exports.getTourStats = catchAsync(async (req, res, next) => {
         {
             $group: {
                 _id: { $toUpper: '$difficulty' },
-                num: { $sum: 1 },
+                numTours: { $sum: 1 },
                 numRatings: { $sum: '$ratingsQuantity' },
                 avgRating: { $avg: '$ratingsAverage' },
                 avgPrice: { $avg: '$price' },
@@ -41,8 +95,8 @@ exports.getTourStats = catchAsync(async (req, res, next) => {
             $sort: { avgPrice: 1 },
         },
         // {
-        //     $match: { _id: { $ne: 'EASY' } },
-        // },
+        //   $match: { _id: { $ne: 'EASY' } }
+        // }
     ]);
 
     res.status(200).json({
@@ -54,7 +108,7 @@ exports.getTourStats = catchAsync(async (req, res, next) => {
 });
 
 exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
-    const year = req.params.year * 1;
+    const year = req.params.year * 1; // 2021
 
     const plan = await Tour.aggregate([
         {
@@ -99,6 +153,8 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     });
 });
 
+// /tours-within/:distance/center/:latlng/unit/:unit
+// /tours-within/233/center/34.111745,-118.113491/unit/mi
 exports.getToursWithin = catchAsync(async (req, res, next) => {
     const { distance, latlng, unit } = req.params;
     const [lat, lng] = latlng.split(',');
@@ -108,7 +164,7 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
     if (!lat || !lng) {
         next(
             new AppError(
-                'Please provide latitude and longitude in the format lat,lng',
+                'Please provide latitude and longitude in the format lat,lng.',
                 400,
             ),
         );
@@ -136,7 +192,7 @@ exports.getDistances = catchAsync(async (req, res, next) => {
     if (!lat || !lng) {
         next(
             new AppError(
-                'Please provide latitude and longitude in the format lat,lng',
+                'Please provide latitude and longitude in the format lat,lng.',
                 400,
             ),
         );
@@ -150,7 +206,7 @@ exports.getDistances = catchAsync(async (req, res, next) => {
                     coordinates: [lng * 1, lat * 1],
                 },
                 distanceField: 'distance',
-                distanceMultiplier: 0.001,
+                distanceMultiplier: multiplier,
             },
         },
         {
